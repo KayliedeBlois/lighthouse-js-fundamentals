2bfc015bbdeb200e5184789db2175e4ffbe60f2b
KayliedeBlois <kayliedeblois@gmail.com>
2022-06-05 20:09:08 -0400

    Use conditionals to tell people about the weather

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -13,11 +13,5 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea...");
-}
 
-if (!raining) {
-  console.log("Leabe your umbrella at home!");
-}
 console.log("Now you're ready to go outside!");
